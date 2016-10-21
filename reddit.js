a4c6af2e3be55d7fb270ab11716be54976b4659d
Sardar Molaei <molaei.s@gmail.com>
2016-10-21 19:27:55 +0000

    completed until votes

--- reddit.js
+++ reddit.js
@@ -4,7 +4,7 @@ var HASH_ROUNDS = 10;
 module.exports = function RedditAPI(conn) {
   return {
     createUser: function(user, callback) {
-      
+
       // first we have to hash the password...
       bcrypt.hash(user.password, HASH_ROUNDS, function(err, hashedPassword) {
         if (err) {
@@ -48,7 +48,7 @@ module.exports = function RedditAPI(conn) {
                       3b. If the insert succeeds, re-fetch the user from the DB
                       4. If the re-fetch succeeds, return the object to the caller
                       */
-                        callback(null, result[0]);
+                      callback(null, result[0]);
                     }
                   }
                 );
@@ -58,9 +58,14 @@ module.exports = function RedditAPI(conn) {
         }
       });
     },
-    createPost: function(post, callback) {
+    createPost: function(post, subredditId, callback) {
+      if (!subredditId) {
+        console.log("subreddit is required")
+      }
+
       conn.query(
-        'INSERT INTO posts (userId, title, url, createdAt) VALUES (?, ?, ?, ?)', [post.userId, post.title, post.url, new Date()],
+        `INSERT INTO posts (userId, title, url, createdAt, subredditId) 
+        VALUES (?, ?, ?, ?, ?)`, [post.userId, post.title, post.url, new Date(), subredditId],
         function(err, result) {
           if (err) {
             callback(err);
@@ -71,7 +76,9 @@ module.exports = function RedditAPI(conn) {
             the post and send it to the caller!
             */
             conn.query(
-              'SELECT id,title,url,userId, createdAt, updatedAt FROM posts WHERE id = ?', [result.insertId],
+              `SELECT id,title,url,userId, createdAt, updatedAt, subredditId 
+               FROM posts 
+               WHERE id = ?`, [result.insertId],
               function(err, result) {
                 if (err) {
                   callback(err);
@@ -93,22 +100,177 @@ module.exports = function RedditAPI(conn) {
       }
       var limit = options.numPerPage || 25; // if options.numPerPage is "falsy" then use 25
       var offset = (options.page || 0) * limit;
-      
+
       conn.query(`
-        SELECT id, title, url, userId, createdAt, updatedAt
-        FROM posts
-        ORDER BY createdAt DESC
-        LIMIT ? OFFSET ?`
-        , [limit, offset],
+        SELECT p.id as postId, 
+        p.title, p.url, 
+        p.createdAt as postCreatedAt, 
+        p.updatedAt as postUpdatedAt, 
+        u.id as usernameId, 
+        u.username, 
+        u.createdAt as userCreatedAt, 
+        u.updatedAt as userUpdatedAt, 
+        s.id as subredditId, 
+        s.name as subredditName, 
+        s.description as subredditDescription, 
+        s.createdAt as subredditCreatedAt, 
+        s.updatedAt as subredditUpdatedAt
+          FROM posts as p
+          JOIN users as u ON u.id = p.userId
+          JOIN subreddit as s ON s.id = p.subredditId
+          GROUP BY postId
+          ORDER BY postCreatedAt DESC;`, [limit, offset],
         function(err, results) {
           if (err) {
             callback(err);
           }
           else {
+            var formatedResults = results.map(function(value) {
+              return {
+                post: {
+                  id: value.postId,
+                  title: value.title,
+                  url: value.url,
+                  createdAt: value.postCreatedAt,
+                  updatedAt: value.postUpdatedAt,
+                },
+                user: {
+                  userId: value.usernameId,
+                  username: value.username,
+                  createdAt: value.userCreatedAt,
+                  updatedAt: value.userUpdatedAt,
+
+                },
+                subreddit: {
+                  subredditId: value.subredditId,
+                  Name: value.subredditName,
+                  description: value.subredditDescription,
+                  createdAt: value.subredditCreatedAt,
+                  updatedAt: value.subredditUpdatedAt
+                }
+              }
+            })
+            callback(null, formatedResults);
+          }
+        }
+      );
+    },
+    getAllPostsForUser: function(userId, options, callback) {
+      if (!callback) {
+        callback = options;
+        options = {};
+      }
+      var limit = options.numPerPage || 25; // if options.numPerPage is "falsy" then use 25
+      var offset = (options.page || 0) * limit;
+
+      conn.query(`
+        SELECT *
+        FROM posts
+        JOIN users
+        ON (users.id = posts.userId) where userId=?`, [userId, limit, offset],
+        function(err, results) {
+          if (err) {
+            callback(err);
+          }
+          else {
+
+            var formatedResults = results.map(function(value) {
+              return {
+                id: value.id,
+                title: value.title,
+                url: value.url,
+                createdAt: value.createdAt,
+                updatedAt: value.updatedAt,
+              }
+            })
             callback(null, results);
           }
         }
       );
+    },
+    getSinglePost: function(postId, callback) {
+      conn.query(
+        `SELECT id,title,url,userId, createdAt, updatedAt 
+          FROM posts 
+          WHERE posts.id = ?`, [postId],
+        function(err, result) {
+          if (err) {
+            callback(err);
+          }
+          else {
+            var formatedResults = result.map(function(value) {
+              return {
+                id: value.id,
+                title: value.title,
+                url: value.url,
+                createdAt: value.createdAt,
+                updatedAt: value.updatedAt,
+              }
+            })
+            callback(null, formatedResults)
+          }
+        }
+      );
+    },
+    createSubreddit: function(sub, callback) {
+      if (!sub || !sub.name) {
+        callback(new Error('name is mandatory'));
+        return;
+      }
+      conn.query(
+        `INSERT INTO subreddit (id, name, description, createdAt,updatedAt)
+         VALUES (?, ?, ?, ?, ?)`, [sub.id, sub.name, sub.description, new Date(), new Date()],
+        function(err, result) {
+          if (err) {
+            callback(err);
+          }
+          else {
+            /*
+            Post inserted successfully. Let's use the result.insertId to retrieve
+            the post and send it to the caller!
+            */
+            conn.query(
+              `SELECT id,title,url,userId, createdAt, updatedAt 
+               FROM posts 
+               WHERE id = ?`, [result.insertId],
+              function(err, result) {
+                if (err) {
+                  callback(err);
+                }
+                else {
+                  callback(null, result[0]);
+                }
+              }
+            );
+          }
+        }
+      );
+    },
+    getAllSubreddits: function(callback) {
+      conn.query(`
+      SELECT id,
+          name,
+          description,
+          createdAt,
+          updatedAt
+        FROM subreddit
+        ORDER BY createdAt DESC`, function(err, result) {
+        if (err) {
+          callback(err);
+        }
+        else {
+         var formatedResults =  result.map(function(ele) {
+            return {
+              id: ele.id,
+              name: ele.name,
+              description: ele.description,
+              createdAt: ele.createdAt,
+              updatedAt: ele.updatedAt
+            }
+          })
+          callback(null, formatedResults);
+        }
+      });
     }
   }
 }
--- reddit.sql
+++ reddit.sql
@@ -3,7 +3,7 @@
 CREATE TABLE `users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(60) NOT NULL, -- why 60??? ask me :)
+  `password` VARCHAR(60) NOT NULL, 
   `createdAt` DATETIME NOT NULL,
   `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
@@ -17,9 +17,84 @@ CREATE TABLE `posts` (
   `title` varchar(300) DEFAULT NULL,
   `url` varchar(2000) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
+  `subredditId` int(11) -- this was added!
   `createdAt` DATETIME NOT NULL,
   `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `userId` (`userId`), -- why did we add this here? ask me :)
+  KEY `userId` (`userId`), 
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL
 );
+
+-- Access getAllPosts 
+
+SELECT *
+        FROM posts
+        JOIN users ON (users.id = posts.userId)
+        JOIN subreddit ON (subreddit.id = posts.subredditId);
+        
+
+-- Get all post for user by User ID
+
+SELECT *
+        FROM posts
+        JOIN users
+        ON (users.id = posts.userId) where userId=?:
+
+-- Get Single post with post ID
+SELECT id,title,url,userId, createdAt, updatedAt 
+        FROM posts 
+        WHERE posts.id = ?
+
+
+-- Get subreddit 
+CREATE TABLE `subreddit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30), -- ALTER TABLE subreddit Alter COLUMN name VARCHAR(30) not null
+  `description` varchar(200),
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+);
+
+
+-- Add subredditId to posts 
+
+
+-- alter post table to add foreign key to subreddit
+
+alter Table posts add subredditId int(11)
+
+ALTER TABLE `posts` ADD 
+      FOREIGN KEY (`subredditId` ) 
+      REFERENCES `subreddit` (`id` ) ON DELETE SET NULL;
+        
+-- Get all subreddits
+
+SELECT * 
+  FROM subreddit 
+  ORDER BY createdAt 
+  DESC
+  
+
+-- making alias for getAllPosts'
+
+
+SELECT  p.id as postId, 
+        p.title, p.url, 
+        p.createdAt as postCreatedAt, 
+        p.updatedAt as postUpdatedAt, 
+        u.id as usernameId, 
+        u.username, 
+        u.createdAt as userCreatedAt, 
+        u.updatedAt as userUpdatedAt, 
+        s.id as subredditId, 
+        s.name as subredditName, 
+        s.description as subredditDescription, 
+        s.createdAt as subredditCreatedAt, 
+        s.updatedAt as subredditUpdatedAt
+          FROM posts as p
+          JOIN users as u ON u.id = p.userId
+          JOIN subreddit as s ON s.id = p.subredditId
+          GROUP BY postId
+          ORDER BY postCreatedAt DESC;
