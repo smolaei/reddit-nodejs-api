1544a3e39543534834c96608904e30eb6a36374d
Sardar Molaei <molaei.s@gmail.com>
2016-10-24 18:42:29 +0000

    created index

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello', function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/hello?name=firstName", function (req, res) {
+  res.send("<h1>Hello _name_!</h1>");
+});
+
+
+
+/* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
+
+// Boilerplate code to start up the web server
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
